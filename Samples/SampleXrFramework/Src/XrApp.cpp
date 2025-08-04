05008624b7889f2b37a7d265759e20cd6ffaacec
migueldulu <miguelangel.duran.luque@alumnos.upm.es>
2025-08-04 14:55:35 +0200

    actualizacion comentarios

--- Samples/SampleXrFramework/Src/XrApp.cpp
+++ Samples/SampleXrFramework/Src/XrApp.cpp
@@ -1,21 +1,4 @@
-/*
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- * All rights reserved.
- *
- * Licensed under the Oculus SDK License Agreement (the "License");
- * you may not use the Oculus SDK except in compliance with the License,
- * which is provided at the time of installation or download, or which
- * otherwise accompanies this software in either electronic or hard copy form.
- *
- * You may obtain a copy of the License at
- * https://developer.oculus.com/licenses/oculussdk/
- *
- * Unless required by applicable law or agreed to in writing, the Oculus SDK
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 /*******************************************************************************
 
 Filename    :   XrApp.cpp
@@ -53,6 +36,7 @@ using OVR::Vector2f;
 using OVR::Vector3f;
 using OVR::Vector4f;
 
+//Convierte códigos  y errores a texto legible
 std::string OXR_ResultToString(XrInstance instance, XrResult result) {
     char errorBuffer[XR_MAX_RESULT_STRING_SIZE]{};
     xrResultToString(instance, result, errorBuffer);
