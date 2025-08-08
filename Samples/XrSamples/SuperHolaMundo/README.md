5d931e22f2dceaa5d4369691860d65d7acc07dd9
migueldulu <miguelangel.duran.luque@alumnos.upm.es>
2025-08-08 19:09:04 +0200

    Ahora SuperHolaMundo tiene un boton que oculta/muestra el texto y se reposiciona

--- Samples/XrSamples/SuperHolaMundo/README.md
+++ Samples/XrSamples/SuperHolaMundo/README.md
@@ -2,6 +2,5 @@
 
 Es un holamundo, pero añadiendo más características:
     - He añadido un icono de aplicacion
-    - Ahora el cartel se centra según la vista del usuario
-    - Mostrar u ocultar cuando se pulsa un boton
-    - (ver si se puede poner fijo)
+    - Ahora el cartel se centra según la vista del usuario al pulsar A
+    - Mostrar u ocultar el texto cuando se pulsa un boton
--- Samples/XrSamples/SuperHolaMundo/Src/main.cpp
+++ Samples/XrSamples/SuperHolaMundo/Src/main.cpp
@@ -14,10 +14,12 @@
 
 class XrAppBaseApp : public OVRFW::XrApp {
 
-private:
+    private:
     OVRFW::VRMenuObject* holaMundoLabel;  // La voy a usar para cambiar la posicion OJO es una ref no un obj
+    OVRFW::VRMenuObject* toggleButton;
     bool debeReposicionar = false;  // Flag para reposicionar
     bool labelCreado = false;
+    bool labelVisible = true;
 
 public:
     // Constructor, establece color de fondo
@@ -31,10 +33,10 @@ public:
         return extensions;
     }
 
-    // Antes de esta función, OVRFW::XrApp::Init() llama:
-    // - xrInitializeLoaderKHR
-    // - xrCreateInstance con las extensiones de GetExtensions,
-    // - xrSuggestInteractionProfileBindings(...) para configurar los enlaces de acciones
+    /* Antes de esta función, OVRFW::XrApp::Init() llama:
+     - xrInitializeLoaderKHR
+     - xrCreateInstance con las extensiones de GetExtensions,
+     - xrSuggestInteractionProfileBindings(...) para configurar los enlaces de acciones */
     virtual bool AppInit(const xrJava* context) override {
         // Init UI system
         if (false == ui_.Init(context, GetFileSys())) {
@@ -44,13 +46,12 @@ public:
         return true;
     }
 
-    // Antes de esta función y dsp de AppInit,  XrApp::InitSession() llama:
-    // - xrCreateSession
-    // - xrCreateReferenceSpace para espacio local y espacio de escenario
-    // - Create swapchain with xrCreateSwapchain
-    // - xrAttachSessionActionSets
+    /* Antes de esta función y dsp de AppInit,  XrApp::InitSession() llama:
+     - xrCreateSession
+     - xrCreateReferenceSpace para espacio local y espacio de escenario
+     - Create swapchain with xrCreateSwapchain
+     - xrAttachSessionActionSets */
     virtual bool SessionInit() override {
-
         // Init objects that need OpenXR Session
         if (!controllerRenderL_.Init(true)) {
             ALOG("SessionInit::Init L controller renderer FAILED.");
@@ -61,7 +62,6 @@ public:
             return false;
         }
         cursorBeamRenderer_.Init(GetFileSys(), nullptr, OVR::Vector4f(1.0f), 1.0f);
-
         return true;
     }
 
@@ -74,6 +74,19 @@ public:
             OVR::Vector3f posiInicial = matrizInicial.Transform({0.0f, -0.35f, -2.0f});
             holaMundoLabel = ui_.AddLabel("Super Hola Mundo", posiInicial, {400.0f, 100.0f});
             holaMundoLabel->SetLocalRotation(in.HeadPose.Rotation);
+
+            //creamos el boton
+            // NUEVO: Crear el botón virtual de toggle
+            OVR::Vector3f posicionBoton = matrizInicial.Transform({-0.75f, -0.1f, -2.0f});
+            toggleButton = ui_.AddButton(
+                    "Ocultar Texto",
+                    posicionBoton,
+                    {200.0f, 75.0f},
+                    [this]() {
+                        this->ToggleTextoVisibilidad();
+                    }
+            );
+            toggleButton->SetLocalRotation(in.HeadPose.Rotation);
             labelCreado = true;
         }
         // Detectar si se presiona botón A para reposicionar (como el botón Meta)
@@ -86,14 +99,8 @@ public:
         }
 
         if (debeReposicionar) {
-            //borramos el padre, que es lo que tomaba setLocalPosition de referencia de posicion
-            ui_.RemoveParentMenu(holaMundoLabel);
-            holaMundoLabel = nullptr;
-            OVR::Matrix4f matrizNuevaCabeza = OVR::Matrix4f(in.HeadPose);
-            OVR::Vector3f nuevaPosi = matrizNuevaCabeza.Transform({0.0f, -0.35f, -2.0f});
-            holaMundoLabel = ui_.AddLabel("Super Hola Mundo", nuevaPosi, {400.0f, 100.0f});
-            holaMundoLabel->SetLocalRotation(in.HeadPose.Rotation);
-
+            //reposicionar label y boton
+            ReposicionarElementos(in.HeadPose);
             debeReposicionar = false;
         }
 
@@ -141,13 +148,64 @@ public:
         ui_.Shutdown();
     }
 
-private:
+    private:
     OVRFW::ControllerRenderer controllerRenderL_;
     OVRFW::ControllerRenderer controllerRenderR_;
     OVRFW::TinyUI ui_;
 
     // Renderer that draws the beam from the controller
     OVRFW::SimpleBeamRenderer cursorBeamRenderer_;
+
+    // NUEVO: Función para alternar la visibilidad del texto
+    void ToggleTextoVisibilidad() {
+        if (holaMundoLabel != nullptr) {
+            labelVisible = !labelVisible;
+            holaMundoLabel->SetVisible(labelVisible);
+
+            // Actualizar el texto del botón según el estado
+            if (labelVisible) {
+                toggleButton->SetText("Ocultar Texto");
+            } else {
+                toggleButton->SetText("Mostrar Texto");
+            }
+        }
+        ALOG("ToggleTextoVisibilidad llamado - labelVisible: %s",
+             labelVisible ? "true" : "false");
+    }
+
+    void ReposicionarElementos(const OVR::Posef& headPose) {
+        if (holaMundoLabel && toggleButton) {
+            /*borramos el padre, que es lo que tomaba setLocalPosition de referencia de posicion y se recolocaba mal
+            se hace en este orden para que el siguiente parent Menu sea el correcto (del label)
+            si coge el parent Menu de crear Button, el label nuevo puede ser clickado */
+            ui_.RemoveParentMenu(toggleButton);
+            ui_.RemoveParentMenu(holaMundoLabel);
+            toggleButton = nullptr;
+            holaMundoLabel = nullptr;
+
+            // Recrear con nueva posición
+            OVR::Matrix4f matrizNuevaCabeza = OVR::Matrix4f(headPose);
+
+            // Recrear el label
+            OVR::Vector3f nuevaPosi = matrizNuevaCabeza.Transform({0.0f, -0.35f, -2.0f});
+            holaMundoLabel = ui_.AddLabel("Super Hola Mundo", nuevaPosi, {400.0f, 100.0f});
+            holaMundoLabel->SetLocalRotation(headPose.Rotation);
+            holaMundoLabel->SetVisible(labelVisible);
+
+            // Recrear el botón
+            OVR::Vector3f posicionBoton = matrizNuevaCabeza.Transform({-0.75f, -0.1f, -2.0f});
+            toggleButton = ui_.AddButton(
+                    // si es true el 1 si es false el segundo
+                    labelVisible ? "Ocultar Texto" : "Mostrar Texto",
+                    posicionBoton,
+                    {200.0f, 75.0f},
+                    [this]() {
+                        this->ToggleTextoVisibilidad();
+                    }
+            );
+            toggleButton->SetLocalRotation(headPose.Rotation);
+        }
+    }
 };
 
 ENTRY_POINT(XrAppBaseApp)
